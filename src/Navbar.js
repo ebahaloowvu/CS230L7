9740a300362772cf7b659e9ff23d8fb8decae608
ebahaloowvu <150372001+ebahaloowvu@users.noreply.github.com>
2024-04-17 01:27:09 -0400

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,57 +3,41 @@ import React from "react";
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="container">
-        <a className="navbar-brand" href="#">
-          Your Brand
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
-          <form className="form-inline">
-            <div className="row">
-              <div className="col">
-                <input
-                  className="form-control mr-sm-2"
-                  type="search"
-                  placeholder="Search"
-                  aria-label="Search"
-                />
-              </div>
-              <div className="col-auto">
-                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
-                  Search
-                </button>
-              </div>
+      <div className="container justify-content-between">
+        <ul className="navbar-nav">
+          <li className="nav-item active">
+            <a className="nav-link" href="#">
+              Home <span className="sr-only">(current)</span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              About
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Contact
+            </a>
+          </li>
+        </ul>
+        <form className="form-inline">
+          <div className="row">
+            <div className="col">
+              <input
+                className="form-control mr-sm-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
             </div>
-          </form>
-          <ul className="navbar-nav">
-            <li className="nav-item active">
-              <a className="nav-link" href="#">
-                Home <span className="sr-only">(current)</span>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                About
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Contact
-              </a>
-            </li>
-          </ul>
-        </div>
+            <div className="col-auto">
+              <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
+                Search
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
     </nav>
   );
