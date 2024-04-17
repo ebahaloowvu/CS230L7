c3992dc8a69f19fbf63d1446dbe87696e6fc512b
ebahaloowvu <150372001+ebahaloowvu@users.noreply.github.com>
2024-04-17 00:54:27 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,16 +6,16 @@ import Card from "./Card";
 function App() {
   return (
     <div className="App">
+    
       <Navbar />
-      <div className="card-container">
-        <Card title="Card 1" content="Content for Card 1" />
-        <Card title="Card 2" content="Content for Card 2" />
-        <Card title="Card 3" content="Content for Card 3" />
-      </div>
-      <h1>CS 230L</h1>
+    <h1>CS 230L</h1>
         <h2>Section - 01</h2>
         <p>WVU ID: 800416929</p>
         <p>Hi, I am Ehsan Bahaloo.</p>
+      <div className="card-container">
+        <Card title="Card 1" content="Content for Card 1" />
+      </div>
+      
     </div>
   );
 }
