6c0df25b65ff249c846ecf6eb51f1a78e41b8c1b
ebahaloowvu <150372001+ebahaloowvu@users.noreply.github.com>
2024-04-17 01:03:13 -0400

    Update Card.js

--- src/Card.js
+++ src/Card.js
@@ -1,29 +1,28 @@
-// Card.js
 import React from "react";
 
 const Card = () => {
   return (
     <div className="row">
       <div className="col-sm-4">
-        <div className="card">
+        <div className="card bg-primary text-white">
           <div className="card-body">
-            <h5 className="card-title">Card 1</h5>
+            <h5 className="card-title mb-4">Card 1</h5>
             <p className="card-text">This is card 1 content.</p>
           </div>
         </div>
       </div>
       <div className="col-sm-4">
-        <div className="card">
+        <div className="card bg-success text-white">
           <div className="card-body">
-            <h5 className="card-title">Card 2</h5>
+            <h5 className="card-title mb-4">Card 2</h5>
             <p className="card-text">This is card 2 content.</p>
           </div>
         </div>
       </div>
       <div className="col-sm-4">
-        <div className="card">
+        <div className="card bg-warning text-dark">
           <div className="card-body">
-            <h5 className="card-title">Card 3</h5>
+            <h5 className="card-title mb-4">Card 3</h5>
             <p className="card-text">This is card 3 content.</p>
           </div>
         </div>
